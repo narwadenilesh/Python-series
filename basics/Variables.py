585ecf39cbf477e291bb126cc9f53df419206d3b
narwadenilesh <nileshnarwade16@gmail.com>
2026-06-15 23:06:50 +0530

    first commit

---
+++ basics/Variables.py
@@ -0,0 +1,34 @@
+# Variables are used to store data that can be referenced and manipulated during program execution. A variable is essentially a name that is assigned to a value.
+
+# x = 5
+# name = "Alex"  
+# print(x)
+# print(name)
+
+
+
+# a = b = c = 100
+# print(a,b,c)
+
+# x, y, z = 1, 2.5, "Python"
+# print(x,y,z)
+
+
+# del keyword is used to delete a variable from memory. After deletion, the variable can no longer be accessed.
+# x = 10
+# del x
+# print(x) 
+
+# type() function in Python
+# x = 10
+# print(type(x))
+
+# Uses type() to compare whether two variables belong to the same data type.
+# x = 10
+# y = 5.5
+# print(type(x) is type(y))
+
+# Insert a Variable into a String - Python
+# a = "Python"
+# res = f"This is {a} programming!"
+# print(res)
